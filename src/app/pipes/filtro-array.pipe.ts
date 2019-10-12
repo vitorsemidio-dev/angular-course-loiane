7742834def2503d697038b4cc504d3e1ffe95d91
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-10-12 12:20:10 -0300

    [Aula 46] Pipe de filtro

---
+++ src/app/pipes/filtro-array.pipe.ts
@@ -0,0 +1,23 @@
+import { MangaModel } from './manga/manga.model';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filtroArray'
+})
+export class FiltroArrayPipe implements PipeTransform {
+
+  transform(value: MangaModel[], ...args: any[]): any {
+
+    if (args.length === 0 || !args) {
+      return value;
+    }
+
+    const filter = args.toLocaleString().toLowerCase();
+    console.log(filter);
+
+    return value.filter(
+      v => v.titulo.toLowerCase().indexOf(filter) !== -1
+    );
+  }
+
+}
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PipesComponent } from './pipes.component';
 import { MangaComponent } from './manga/manga.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @NgModule({
   declarations: [
     PipesComponent,
     MangaComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
     CommonModule
